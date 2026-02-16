56000cc344552f7d48fe6628fdb3f89fe4720c5d
m0pies <holodkov.dsgn@gmail.com>
2026-02-16 17:12:38 +0700

    performance optimization

--- app/page.js
+++ app/page.js
@@ -1,8 +1,4 @@
-import dynamic from "next/dynamic";
-const Hero = dynamic(() => import("./components/Hero3d"), {
-  ssr: false,
-});
-
+import Hero from "./components/Hero3d";
 import Work from "./components/Work";
 import Services from "./components/Services";
 import About from "./components/About";
