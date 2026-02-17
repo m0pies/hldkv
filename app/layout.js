7d9a7f39368f9f06779275ed467b0ec01cfc3172
m0pies <holodkov.dsgn@gmail.com>
2026-02-17 19:48:05 +0700

    fonts

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,6 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { GeistSans } from 'geist/font/sans';
 import "./globals.css";
 import BottomNav from "./components/BottomNav";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "hldkv design",
@@ -21,7 +11,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={GeistSans.variable}
         style={{ backgroundColor: '#0d0d0c' }}
       >
         <BottomNav />
