ff4d0dabeff19cec9ba4219bae04803c350a2a5a
m0pies <holodkov.dsgn@gmail.com>
2026-02-18 22:52:30 +0700

    loade almost ready

--- app/components/Hero3d.jsx
+++ app/components/Hero3d.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Canvas, useThree } from "@react-three/fiber";
-import { useProgress, Environment, OrbitControls } from "@react-three/drei";
+import { useProgress, Environment } from "@react-three/drei";
 import { Suspense, useRef, useEffect, useState } from "react";
 import Model from "../Model";
 import BgText from "../BgText";
@@ -9,8 +9,53 @@ import BgText from "../BgText";
 export default function Hero3d() {
     const containerRef = useRef(null);
     const [isTouchDevice, setIsTouchDevice] = useState(false);
-    const { progress, active } = useProgress();
+    
+    const { active } = useProgress();
+    const [progress, setProgress] = useState(0);
+    const [isFinished, setIsFinished] = useState(false);
+    const [hideLoader, setHideLoader] = useState(false);
 
+    const [loaderFinished, setLoaderFinished] = useState(false);
+
+    useEffect(() => {
+        if (progress >= 100) {
+            setIsFinished(true);
+
+            setTimeout(() => {
+            setHideLoader(true);
+            setLoaderFinished(true);
+            }, 400);
+        }
+    }, [progress]);
+
+
+    useEffect(() => {
+        let frame;
+
+        const update = () => {
+            setProgress((prev) => {
+            if (prev >= 100) return 100;
+
+            return prev + 1;
+            });
+
+            frame = requestAnimationFrame(update);
+        };
+
+        frame = requestAnimationFrame(update);
+
+        return () => cancelAnimationFrame(frame);
+    }, []);
+
+    useEffect(() => {
+        if (progress >= 100) {
+            setIsFinished(true);
+
+            setTimeout(() => {
+            setHideLoader(true);
+            }, 400);
+        }
+    }, [progress]);
 
     useEffect(() => {
         const hasTouch =
@@ -26,16 +71,22 @@ export default function Hero3d() {
             className="relative h-screen w-full"
         >
             
-            {active && (
-                <div className="absolute inset-0 z-50 flex items-center justify-center bg-[#0D0D0C]">
-                    <div className="flex flex-col items-center gap-4">
-                    <div className="h-10 w-10 animate-spin rounded-full border-4 border-white/20 border-t-white" />
-                    <p className="text-xs tracking-widest text-white/60">
-                        {Math.floor(progress)}%
-                    </p>
+            {!hideLoader && (
+                <div
+                    className="fixed inset-0 z-[999] flex items-center justify-center bg-[#0D0D0C] transition-opacity"
+                    style={{
+                    opacity: isFinished ? 0 : 1,
+                    pointerEvents: isFinished ? "none" : "auto",
+                    }}
+                >
+                    <div className="text-white/50 text-sm">
+                    {progress}%
                     </div>
                 </div>
             )}
+
+
+
 
             <Canvas
                 dpr={[1,1.5]}
@@ -53,14 +104,14 @@ export default function Hero3d() {
 
                 <Environment preset="studio" />
 
-                <BgText />
+                
+                {loaderFinished && <BgText />}
 
                 <ambientLight intensity={0.25} />
                 <directionalLight position={[5, 10, 5]} intensity={3.5} />
 
-                <Model />
+                {loaderFinished && <Model />}
 
-                {/* <OrbitControls enableRotate={false} enableZoom={false} enablePan={false} pointerEvents={false} /> */}
             </Canvas>
         </section>
     );
