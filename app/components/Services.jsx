854de42864a42d7c5b2c3046205cb50d6cf9ba27
m0pies <holodkov.dsgn@gmail.com>
2026-02-22 14:03:26 +0700

    services h

--- app/components/Services.jsx
+++ app/components/Services.jsx
@@ -104,7 +104,7 @@ export default function ServicesSection() {
         <section
             id="services"
             ref={ref}
-            className="bg-[#0D0D0C] text-white"
+            className="bg-[#0D0D0C] text-white min-h-[100vh]"
             style={{ height: `${services.length * 100}vh` }}
         >
             <div className="sticky top-0 h-screen">
