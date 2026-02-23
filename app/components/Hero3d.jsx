594e02ec972502bad425f56d5bf0f17ae4724e0a
m0pies <holodkov.dsgn@gmail.com>
2026-02-23 13:45:35 +0700

    PERFECT

--- app/components/Hero3d.jsx
+++ app/components/Hero3d.jsx
@@ -5,12 +5,6 @@ import { Environment } from "@react-three/drei";
 import { Suspense, useRef, useEffect, useState } from "react";
 import Model from "../Model";
 import BgText from "../BgText";
-import { motion } from "framer-motion";
-
-const fadeOutVariants = {
-  visible: { opacity: 1 },
-  hidden: { opacity: 0 },
-};
 
 export default function Hero3d() {
     const containerRef = useRef(null);
@@ -28,20 +22,6 @@ export default function Hero3d() {
             id="hero"
             className="relative h-screen w-full"
         >
-            <motion.div className="flex justify-center absolute inset-0 z-100 items-center"
-            variants={fadeOutVariants}
-        initial="hidden"
-        animate="visible"
-        transition={{
-          duration: 0.8,
-          delay: 3,
-          ease: "easeInOut",
-        }}>
-                <div className="px-5 sm:px-6 lg:px-4 w-full md:max-w-[1024px] lg:max-w-[1200px] flex items-center justify-between pt-20 sm:pt-24 md:pt-32 lg:pt-42 h-20">
-                    <span className="text-[#7d7d7d] font-light text-xs sm:text-base md:text-lg lg:text-xl">From idea to strcuture</span>
-                    <span className="text-[#7d7d7d] font-light text-xs sm:text-base md:text-lg lg:text-xl">From structure to product</span>
-                </div>
-            </motion.div>
             <Canvas
                 dpr={[1, 1.5]}
                 camera={{ position: [0, 0, 5], fov: 50 }}
